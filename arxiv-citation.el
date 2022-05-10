slotThe/arXiv-citation

commit 573c49578d5b99bda94db12131ca0ae3da47f99e

Surround capital letters with {} in the title
Otherwise, bibtex may downcase them—we don't want that.

arxiv-citation.el
@@ -163,9 +163,12 @@ Returns a plist of with keywords `:id', `:authors', `:title',
   (let* ((arXiv-id (arxiv-citation-arXiv-id link))
          (url (format "http://export.arxiv.org/api/query?id_list=%s" arXiv-id)))
     (with-current-buffer (url-retrieve-synchronously url t t)
+      (setq case-fold-search nil)       ; Regexps should be case sensitive.
       (let* ((xml (arxiv-citation-parse :xml))
              (entry (alist-get 'entry xml))
-             (title (s-replace "\n" "" (cadr (alist-get 'title entry))))
+             (title (->> (cadr (alist-get 'title entry))
+                         (s-replace "\n" "")
+                         (s-replace-regexp "\\([A-Z]\\)" "{\\&}")))
              (authors_ (->> entry
                             (--filter (string= (car it) 'author))
                             (-map (-compose #'caddr #'caddr))

commit a7e79919aa44c495c4fd10fd801e3b1225207446

Remove {} in PDF title
Fixes: 573c49578d5b99bda94db12131ca0ae3da47f99e

arxiv-citation.el
@@ -139,8 +139,8 @@ The output name is of the following form:
             (->> (plist-get info :title)
                  downcase
                  (s-replace-all '(("_" . "-") (" " . "-")
-                                  ("$" . "") ("," . "")
-                                  ("\\" . "")))))))
+                                  ("$" . "") ("," . "") ("\\" . "")
+                                  ("{" . "") ("}" . "")))))))
 
 (defun arxiv-citation-generate-autokey ()
   "Generate a key for a bibtex entry in the current buffer.